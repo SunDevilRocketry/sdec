a64b56dcd02f94a605221b89576535b827f8a9c7
Colton Acosta <cacost12@asu.edu>
2022-04-21 08:48:49 -0700

    deleted commented code

--- terminal.py
+++ terminal.py
@@ -8,23 +8,11 @@ import serial.tools.list_ports
 # SDR modules
 import commands # functions for each terminal command
 
-#serialPortDir = '/dev/ttyUSB1'
-#baudrate = 9600
-#serialDataObj = serial.Serial(serialPortDir, baudrate=baudrate, timeout = 1)
-#while True:
-    #data = serialDataObj.readline()
-    #data = data.decode("UTF-8")
-    #data = data.strip()
-    #if (data != ""):
-    #    print(data)
-#    serialDataObj.write(b'1');
-#    time.sleep(1)
-
 # Program Loop
 while(True):
     # Command prompt
     userin = input("> ")
 
-    # Parse and execute command
+    # Parse and eecute command
     commands.parseInput(userin)
 
