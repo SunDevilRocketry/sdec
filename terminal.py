8aa77ad868761eaa576a1c19704af4512475316c
Colton Acosta <cacost12@asu.edu>
2022-05-08 11:14:49 -0700

    added global serial port object for sharing info between command funcs

--- terminal.py
+++ terminal.py
@@ -8,6 +8,59 @@ import serial.tools.list_ports
 # SDR modules
 import commands # functions for each terminal command
 
+# Serial Port Object
+# Used to access a serial port and pass serial information
+# to terminal command functions
+class terminalData:
+    def __init__(self):
+        self.baudrate = None
+        self.comport = None
+        self.timeout = None
+        self.serialObj = serial.Serial()
+        self.config_status = False 
+
+    # Configure Serial Port
+    def configComport(baudrate, comport, timeout):
+        self.baudrate = baudrate 
+        self.comport = comport 
+        self.timeout = timeout 
+        self.config_status = True
+
+    # Open the serial port
+    def openComport():
+        # Ensure serial port has been properly configured 
+        if(not self.config_status):
+            print("Error: Cannot open serial port. Serial port has not been properly configured")
+            return
+
+        # open port
+        self.serialObj.open()
+
+    # Close the serial port
+    def closeComport():
+        # check that the serial port is open
+        if (not self.serialObj.is_open):
+            print("No open serial port detected")
+        else:
+            self.serialObj.close()
+
+    # Write a single Byte to the serial port
+    def sendByte(data):
+        if (not self.serialObj.is_open):
+            print("Error: Could not transmit byte over serial port. No active" \
+                   +"serial port connection")
+        else:
+            self.serialObj.write(data)
+
+    # Read a single Byte from the serial port
+    def readByte():
+        if (not self.serialObj.is_open):
+            print("Error: Could not read byte from serial port. No active" \
+                   +"serial port connection")
+        else:
+             return self.serialObj.read()
+
+
 # parseInput -- checks user input against command list 
 #               options
 # input: userin: user inputed string
