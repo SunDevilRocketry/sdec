ad123a13a191e69fb499625eaf233837d7142e1b
Colton Acosta <cacost12@asu.edu>
2022-06-07 18:08:42 -0700

    added basic sol parameter parsing

--- valveController.py
+++ valveController.py
@@ -14,4 +14,70 @@ import serial.tools.list_ports
 
 # sol -- control solenoids states
 def sol(Args, serialObj):
+	###########################################################
+	# local variables                                         #
+    ###########################################################
+
+	# Subcommand list
+	subcommands = [ 'on'    , \
+                    'off'   , \
+                    'toggle', \
+                    'reset'     ]
+    
+	# Options list
+	options =     [ '-h', \
+                    '-n'    ]
+
+    # Option descriptions dictionary
+	option_descriptions = {
+		'-h' : 'Display help info',
+		'-n' : 'Specify a solenoid number'
+	}
+
+	###########################################################
+	# function inputs parsing                                 #
+    ###########################################################
+
+	# no subcommands/options
+	if (len(Args) == 0):
+		print('Error: No subcommand supplied. Valid subcommands include: ')
+		for subcommand in subcommands:
+			print('\t' + subcommand)
+		print()
+		return serialObj
+
+	# too many inputs
+	elif (len(Args) > 3): 
+		print('Error: To many inputs.')
+		return serialObj
+
+	# unrecognized subcommand
+	elif (not (Args[0] in subcommands)): 
+		print('Error: Unrecognized subcommand. Valid subcommands include: ')
+		for subcommand in subcommands:
+			print('\t' + subcommand)
+		print()
+		return serialObj
+
+	# unrecognized option
+	elif (not(Args[1] in options)): 
+		print('Error: Unrecognized option. Valid options include: ')
+		for option in options:
+			print('\t' + option + '\t' + option_descriptions[option]) 
+		print()
+		return serialObj
+
+	# not connected to valve controller
+	elif ()
+		# solenoid number out of bounds
+
+    # Help option
+
+    # sol on
+
+	# sol off
+
+    # sol toggle
+
+	# sol reset
 	return
