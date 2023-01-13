f6cb53eaa8d4d6dfd4078ec18b1f6e26de4e2a7a
Colton Acosta <cacost12@asu.edu>
2023-01-12 17:13:59 -0700

    added if name is main protection to sdec main program loop

--- sdec.py
+++ sdec.py
@@ -204,31 +204,32 @@ def parseInput(userin):
 ####################################################################################
 # Application Entry Point                                                          #
 ####################################################################################
+if __name__ == '__main__':
+    
+    # Initialize Serial Port Object
+    terminalSerObj = terminalData()
 
-# Initialize Serial Port Object
-terminalSerObj = terminalData()
+    # Look for possible connections
+    avail_ports = serial.tools.list_ports.comports()
+    for port_num, port in enumerate( avail_ports ):
+        if ( 'CP2102' in port.description ):
+            # Connect
+            port_num = port.device
+            connect_args  = [ '-p', port_num]
+            commands.connect( connect_args, terminalSerObj )
+            
+    # Display command prompt
+    while(True):
+        # Command prompt
+        userin         = input("SDR>> ")
 
-# Look for possible connections
-avail_ports = serial.tools.list_ports.comports()
-for port_num, port in enumerate( avail_ports ):
-    if ( 'CP2102' in port.description ):
-        # Connect
-        port_num = port.device
-        connect_args  = [ '-p', port_num]
-        commands.connect( connect_args, terminalSerObj )
-        
-# Display command prompt
-while(True):
-    # Command prompt
-    userin         = input("SDR>> ")
+        # Parse command
+        userin_clean   = parseInput(userin)
+        userCommand    = userin_clean[0]
+        userArgs       = userin_clean[1:]
 
-    # Parse command
-    userin_clean   = parseInput(userin)
-    userCommand    = userin_clean[0]
-    userArgs       = userin_clean[1:]
-
-    # Execute Command
-    terminalSerObj = command_list[userCommand](userArgs, terminalSerObj)
+        # Execute Command
+        terminalSerObj = command_list[userCommand](userArgs, terminalSerObj)
 
 
 ####################################################################################
