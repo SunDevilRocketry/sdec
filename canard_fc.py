7ee4f3d355f51df8693a40b2fd57d6a15c7e5ff1
niekky <trungkiencfc@gmail.com>
2024-10-18 02:05:06 -0700

    Modified state opcode

--- canard_fc.py
+++ canard_fc.py
@@ -5,27 +5,27 @@ import time
 
 def idle(Args, serialObj):
     print("IDLE")
-    serialObj.sendByte(b'\x01')
+    serialObj.sendByte(b'\x20')
     return serialObj
 
 def imu_calibrate(Args, serialObj):
     print("IMU CALIBRATING")
-    serialObj.sendByte(b'\x04')
+    serialObj.sendByte(b'\x22')
     return serialObj
 
 def pid_run(Args, serialObj):
     print("PID RUN")
-    serialObj.sendByte(b'\x05')
+    serialObj.sendByte(b'\x23')
     return serialObj
 
 def pid_setup(Args, serialObj):
     print("PID SETUP")
-    serialObj.sendByte(b'\x06')
+    serialObj.sendByte(b'\x24')
     return serialObj
 
 def terminal_access(Args, serialObj):
     print("TERMINAL")
-    serialObj.sendByte(b'\x07')
+    serialObj.sendByte(b'\x25')
     return serialObj
 
 
@@ -33,7 +33,7 @@ def fin_setup(Args, serialObj):
     print("FIN Setup")
     print("CONTROLS: j/k RIGHT-/+, f/d LEFT-/+")
     print("Press q to exit")
-    serialObj.sendByte(b'\x03')
+    serialObj.sendByte(b'\x21')
     # serialObj.sendByte(b'\x05')
 
 
