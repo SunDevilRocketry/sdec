6be95abf9b02f2058a09c55ae6465348e2d258fa
sanguy06 <sara.nguyen1035@gmail.com>
2024-11-14 13:56:38 -0700

    Code cleaning

--- parser.py
+++ parser.py
@@ -1,8 +1,4 @@
-
-
 import pandas as pd
-
-
 
 labels = ["save_bit","accel_x_offset","accel_y_offset","accel_z_offset","gyro_x_offset","gyro_y_offset","gyro_z_offset","rp_servo1","rp_servo2","time","accX","accY","accZ","gyroX","gyroY","gyroZ","magX","magY","magZ","imut","accXconv","accYconv","accZconv","gyroXconv","gyroYconv",
         "gyroZconv","rollDeg","pitchDeg","rollRate","pitchRate","velo","pos","pres","temp"]
@@ -19,12 +15,6 @@ def converter(txt_File, output_File):
             number = line.split('\t')
             data.append(number[:-1])   
 
-    #with open('output.csv', 'w', newline='') as file:
-        #writer = csv.writer(file)
-        #writer.writerows(data)
-    #pd.DataFrame
-    #input(len(data[1]))
-    #input(len(labels))
     df = pd.DataFrame.from_records(data, columns=labels)
     df.to_csv('output.csv',index=False)
 
