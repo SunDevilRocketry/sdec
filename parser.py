352413e4d3438ee0fcf4c2716f9bcae72c4974b2
niekky <trungkiencfc@gmail.com>
2024-11-15 14:37:33 -0700

    Cleaned code

--- parser.py
+++ parser.py
@@ -1,7 +1,46 @@
 import pandas as pd
 
-labels = ["save_bit","accel_x_offset","accel_y_offset","accel_z_offset","gyro_x_offset","gyro_y_offset","gyro_z_offset","rp_servo1","rp_servo2","time","accX","accY","accZ","gyroX","gyroY","gyroZ","magX","magY","magZ","imut","accXconv","accYconv","accZconv","gyroXconv","gyroYconv",
-        "gyroZconv","rollDeg","pitchDeg","rollRate","pitchRate","velo","pos","pres","temp"]
+labels = [
+            "save_bit",
+            "acc_launch_flag",
+            "accel_x_offset",
+            "accel_y_offset",
+            "accel_z_offset",
+            "gyro_x_offset",
+            "gyro_y_offset",
+            "gyro_z_offset",
+            "pres_offset",
+            "pres_temp_offset",
+            "rp_servo1",
+            "rp_servo2",
+            "time",
+            "accX",
+            "accY",
+            "accZ",
+            "gyroX",
+            "gyroY",
+            "gyroZ",
+            "magX",
+            "magY",
+            "magZ",
+            "imut",
+            "accXconv",
+            "accYconv",
+            "accZconv",
+            "gyroXconv",
+            "gyroYconv",
+            "gyroZconv",
+            "rollDeg",
+            "pitchDeg",
+            "rollRate",
+            "pitchRate",
+            "velo",
+            "pos",
+            "pres",
+            "temp",
+            "baro_alt",
+            "baro_velo"
+          ]
 data = []
 
 
