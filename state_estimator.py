313a3a5ccd96cfd2362f07fdbb17f990aa3082f3
ETSsound <147210601+ETSsound@users.noreply.github.com>
2025-03-11 13:17:33 -0700

    still dont trust stashes again

--- state_estimator.py
+++ state_estimator.py
@@ -11,7 +11,7 @@
 
 # IMPORTS
 import os
-import csv
+import math
 import numpy as np
 
 import controller
@@ -19,9 +19,9 @@ import controller
 # GLOBAL VARIABLES
 
 # FORMAT: Take the first four words of the sensor data output.
-supported_platforms = [
-    ["flight_comp_rev2_sensor", controller.controller_names[4]]
-]
+supported_platforms = {
+    "flight_comp_rev2_sensor": controller.controller_names[4]
+}
 
 ######################################################################
 #   Supporting Functions                                             #
@@ -59,14 +59,14 @@ def get_sensor_output_file():
             print("Invalid input. Try again.")
         else:
             validInput = True
-            return ["./output/" + outputFiles[selection - 1], platform]
+            return "./output/" + outputFiles[selection - 1]
     return 
 
 # Retrieve the platform for a given file output
 def get_platform( file ):
     for platform in supported_platforms:
-        if file.find( platform[0] ) > -1:
-            return platform[1]
+        if file.find( platform ) > -1:
+            return supported_platforms[platform]
     return
 
 def parse_data_file( sensor_data_file ):
@@ -99,7 +99,7 @@ def convert_outputs( sensorOutputs, platform ):
     #for i in range(len(sensorOutputs)):
     #    print(len(sensorOutputs[i]))
 
-    for i in range(len(sensorOutputs[0])):
+    for i in range(len(sensorOutputs)):
         conv['time'].append(sensorOutputs[i][2])
 
     for i in range(len(sensorOutputs)):
@@ -108,7 +108,56 @@ def convert_outputs( sensorOutputs, platform ):
 
     return conv
 
+# Returns list of values corresponding to each time
+def get_velocity_from_position( columns ):
+    time = columns["time"]
+    accX = columns["accX"]
+    accY = columns["accY"]
+    accZ = columns["accZ"]
 
+    # Calculate the velocity: deltaV = deltaT * a
+    velocity = {}
+    velocity['time'] = time
+    velocity['velX'] = [0]
+    for i in range(1, len(time)):
+        delta = columns["time"][i] - columns["time"][i - 1]
+        velocity['velX'].append(delta * columns["accX"][i])
+    velocity['velY'] = [0]
+    for i in range(1, len(time)):
+        delta = columns["time"][i] - columns["time"][i - 1]
+        velocity['velY'].append(delta * columns["accY"][i])
+    velocity['velZ'] = [0]
+    for i in range(1, len(time)):
+        delta = columns["time"][i] - columns["time"][i - 1]
+        velocity['velZ'].append(delta * columns["accZ"][i])
+
+    velocity['vel'] = [0]
+    for val in range(len(velocity['velX'])):
+        velocity['vel'].append( math.sqrt( (velocity['velX'][val] ** 2) + (velocity['velY'][val] ** 2) + (velocity['velZ'][val] ** 2) ) )
+
+    return velocity
+
+def get_displacement_from_velocity( velocity ):
+    # Calculate the velocity: deltaV = deltaT * a
+    displacement = {}
+    displacement['dispX'] = [0]
+    for i in range(1, len(velocity['time'])):
+        delta = velocity["time"][i] - velocity["time"][i - 1]
+        displacement['dispX'].append(delta * velocity["velX"][i])
+    displacement['dispY'] = [0]
+    for i in range(1, len(velocity['time'])):
+        delta = velocity["time"][i] - velocity["time"][i - 1]
+        displacement['dispY'].append(delta * velocity["velY"][i])
+    displacement['dispZ'] = [0]
+    for i in range(1, len(velocity['time'])):
+        delta = velocity["time"][i] - velocity["time"][i - 1]
+        displacement['dispZ'].append(delta * velocity["velZ"][i])
+
+    displacement['disp'] = [0]
+    for val in range(len(velocity['time'])):
+        displacement['disp'].append( math.sqrt( (displacement['dispX'][val] ** 2) + (displacement['dispY'][val] ** 2) + (displacement['dispZ'][val] ** 2) ) )
+
+    return displacement
 
 
         
@@ -117,9 +166,8 @@ def convert_outputs( sensorOutputs, platform ):
 def state_estimator( userArgs, terminalSerObj ):
     # Get parameters for estimation
     print( "STATE ESTIMATION: \n")
-    output = get_sensor_output_file()
-    sensor_data_file = output[0]
-    platform = output[1]
+    sensor_data_file = get_sensor_output_file()
+    platform = get_platform(sensor_data_file)
     print("Selection: " + sensor_data_file)
 
     print("Parsing file...")
@@ -132,19 +180,25 @@ def state_estimator( userArgs, terminalSerObj ):
     print("1 - All outputs")
     print("2 - Velocity from Acceleration")
     print("3 - Position from Acceleration")
-    print("4 - Exit")
+    print("q - Exit")
 
     exit = False
     while not exit:
-        selection = int(input("Select option: "))
+        selection = input("Select option: ")
         match selection:
             case 1:
-                print(columns)
+                velocity = get_velocity_from_position( columns )
+                displacement = get_displacement_from_velocity( velocity )
+                print(velocity)
+                print(displacement)
 
             case 2:
-                hAL = []
+                print( get_velocity_from_position( columns ) )
 
-            case 4:
+            case 3:
+                print( get_displacement_from_velocity( get_velocity_from_position( columns ) ) )
+
+            case 'q':
                 exit = True
 
             case _:
